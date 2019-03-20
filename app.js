d6dcf9f02a794126a10ad31e02eab2ca1915cac7
20136208 MARIO ARMANDO MARTINEZ BARAJAS <20136208@TELEMATICA.UCOL.MX>
2019-03-20 08:08:11 -0600

    [ADD] Clase 7

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
-let person = {
-    firtsName: '',
-    lastName: '',
-    greet: function(){
-        return this.firtsName + ' '+this.lastName;
-    }
-}
-
-/*Los objetos john y jane no contienenn ningun atributo, sin embargo, al heredar del objeto person,
-obtienen los atributos*/
-
-let john = Object.create(person);
-john.firtsName = 'John';
-john.lastName = 'Walker';
-
-let jane = Object.create(person);
-jane.firtsName = 'Jane';
-jane.lastName = 'Foster';
-
-console.log(john.greet());
-console.log(jane.greet());
-
-//=========================================================================================================0
-
-/*El objeto Greetr solo contiene el atributo greetign, sin embargo con la cadena de prototipado 
-este objeto hereda las propiedades del objeto EventEmitter, el cual es un objeto que usa la libreria 'events*/
-
 let EventEmitter = require('events');
 let utils = require('util');
 
 
 function Greetr(){
+    EventEmitter.call(this);
     this.greeting = 'Hello World ';
 }
 
@@ -47,18 +21,3 @@ greet1.on('greet', function(data){
 });
 
 greet1.greet('Tony');
-
-//=====================================================================================================================
-
-let obj = {
-    name: 'John Doe',
-    greet: function(){
-        console.log(`Hello ${this.name}`)
-    }
-}
-
-
-//Con los metodos call y apply permiten modificar las propiedades del objeto
-obj.greet();
-obj.greet.call({name: 'John Doe'});
-obj.greet.apply({name: 'Jane Doe'});
