8010b97b8cb3fe7295a7016f3fe7e31c41beb2c4
20136208 MARIO ARMANDO MARTINEZ BARAJAS <20136208@TELEMATICA.UCOL.MX>
2019-04-01 08:11:53 -0600

    [ADD] Basic HTTP Server

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-'use strict';
-//Ejercicio con clases ES6
-let EventEmitter = require('events');
-let util = require('util');
+//HTTP basic server
 
-class Greetr extends EventEmitter{ //Hereda funciones de EventEmitter
-    constructor(){
-        super(); //Se manda a llamar al super constructor
-        this.greeting = 'Hello World'; //Atributo de la clase Greetr
-    }
-    greet(data){
-        this.on('greet', function(data){ //Metodo heredado de greetr
-            console.log('Metodo heredado');
-        })
-        console.log(`${this.greeting}: ${data}`);
-        this.emit('greet', data);
-    }
-}
+let http = require('http');
 
-let greetr = new Greetr();
-greetr.greet('Mario');
+http.createServer(function(req, res){
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});//Header of request
+    res.end('Hello World\n'); //Body of request
+
+}).listen(1337, '127.0.0.1');
