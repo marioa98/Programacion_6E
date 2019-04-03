c9d66924daa32e72d3ef66eb7ee487cca99f32c1
marioa98 <mmartinez53@ucol.mx>
2019-04-03 07:45:42 -0600

    [ADD] Clase 10. Uso de JS con HTML

---
+++ app.js
@@ -0,0 +1,13 @@
+let http = require('http');
+let fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    let html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    let message = 'Otro mensaje';
+    html = html.replace('{Message}', message);
+    res.end(html);
+}).listen(1337, '127.0.0.1');
