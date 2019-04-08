20cef5bba4375c39b250afa48e801a9bcc5ad35e
20136208 MARIO ARMANDO MARTINEZ BARAJAS <20136208@TELEMATICA.UCOL.MX>
2019-04-08 08:26:46 -0500

    [ADD] Clase 11 Routing

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 //HTTP basic server
 
 let http = require('http');
+let fs = require('fs');
 
 http.createServer(function(req, res){
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});//Header of request
-    res.end('Hello World\n'); //Body of request
+    if(req.url === '/'){
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+    }else if(req.url === '/api'){
+        res.writeHead(200, {'Content-Type': 'application/json'});//Header of request
+        let obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        res.end(JSON.stringify(obj)); //Body of request
+    }else{
+        res.writeHead(404);
+        res.end();
+    }
 
+    
 }).listen(1337, '127.0.0.1');
